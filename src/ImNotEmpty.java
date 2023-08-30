ebd661d393d5a6e6576fb5093d47f53f2ec6e80f
PAAG <keapaagdat23a@gmail.com>
2023-08-30 10:29:09 +0200

    if

--- src/ImNotEmpty.java
+++ src/ImNotEmpty.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
 public class ImNotEmpty {
+  public static void main(String[] args) {
+    int age;
+    Scanner in = new Scanner(System.in);
+    System.out.print("What is your age? ");
+    age = in.nextInt();
+    in.nextLine(); // Scanner bug
+
+    if (age < 18) {
+      System.out.print("You're too young.");
+    } else {
+      System.out.print("Welcome to Monkey Bar!");
+    }
+
+    System.out.print("\n\nWhat is your name? ");
+    String name = in.nextLine();
+
+    if (name.equals("") || age > 18) {
+      System.out.print("Hey you!");
+    } else {
+      System.out.print("Hey, " + name);
+    }
+
+    // Exclusive OR, kaldet XOR
+    if (age < 10 ^ age >= 18)
+  }
 }

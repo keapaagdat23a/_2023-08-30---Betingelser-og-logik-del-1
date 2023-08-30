d8e6bcf0e880b08dd30c57cbd3c7b71983e07c0d
PAAG <keapaagdat23a@gmail.com>
2023-08-30 11:01:46 +0200

    while og tilfældige tal

--- src/WhileDemo.java
+++ src/WhileDemo.java
@@ -1,2 +1,22 @@
+import java.util.Random;
+
 public class WhileDemo {
+  public static void main(String[] args) {
+    Random random = new Random();
+    int number = 0;
+
+    // What NOT do to:
+    number = random.nextInt(21) - 10;
+    if (number == 0) {
+      number = random.nextInt(21) - 10;
+      if (number == 0) {
+        number = random.nextInt(21) - 10;
+      }
+    }
+
+    // What TO do:
+    while (number == 0) {
+      number = random.nextInt(21) - 10;
+    }
+  }
 }
